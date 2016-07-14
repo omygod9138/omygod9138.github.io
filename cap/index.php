efb59063dffdf7b7aa6637d25396fa5cf26e8b9f
John <omygod9138@users.noreply.github.com>
2016-07-14 15:59:12 +0800

    Update index.php
    confirmed version

--- cap/index.php
+++ cap/index.php
@@ -1,7 +1,14 @@
 <?php
+include "../Scripts/link.php";
 $file = file_get_contents('caption.txt');
 $str = array_filter(explode(PHP_EOL, $file));
 $display = $_GET['captionMode'] ? $_GET['captionMode'] : null;
+$station = isset($_GET['station']) && trim($_GET['station']) != '' ? trim($_GET['station']) : 1;
+//$hour = date('i') >= 5 ? date('H') - 1 : date('H') - 2;
+$sql =	"SELECT TRUNCATE(Mvalue,2) FROM hour_data WHERE MStation = ".$station." AND MDate =  (SELECT max(Mdate) FROM hour_data WHERE MStation = ".$station.")";
+$result=mysql_db_query($dblink,$sql,$link) or die ("錯誤<br>".$sql); 
+$db_data = mysql_fetch_row($result);
+
 if(!$display){
 ?>
 <!DOCTYPE html>
@@ -9,78 +16,120 @@ if(!$display){
 <head>
 	<meta charset="utf-8">
 	<title>花蓮縣環境保護局</title>
-	<!--<link rel="stylesheet" href="normalize.css" />-->
 	<style>
 		body{
 			width:1920px;
-			height:1080px;
 			background-color:black;
 			color:white;
 			overflow:hidden;
 			white-space: nowrap;
 		}
 		#caption{
-			width:100%;
+			left:1920px;
+			height:1080px;
 			position:absolute;
 			text-align:center;
-			letter-spacing: 5px;
-			line-height: 2;
-			font-size:114px;
 		}
 		.sections{
-			height: 1080px;
-			padding-top: 192px;
+			min-width:1920px;
+			height:850px;
+			text-align:center;
+			vertical-align:middle;
+			display: table-cell;
+			font-size:9cm;
+			position: relative;
 		}
-	
 		img{
-			vertical-align: middle;
-			max-height: 146px;
-			max-width: 203px;
-			margin-top: -36px;
+			vertical-align:middle;
 		}
 	</style>
 	<script src="jquery-1.12.4.min.js"></script>
 	<script>
+		var sect = 1;
+		var width = 0;
 		
-		$(document).ready(function(){
-			var sect = 1
-			var cur_sect;
-			var max_sect = document.querySelectorAll("#caption > div").length;
-			console.log(max_sect);
-			 $('body').bind('mousewheel', function(e){
-				if(e.originalEvent.wheelDelta /120 > 0) {
+		function refresh(){
+			console.log('refreshed');
+			var today = new Date();
+			var currentMin = today.getMinutes() * 60 + today.getSeconds();
+			var currentSec = today.getSeconds();
+			var refresh_Min = 300;
+			if(currentMin >= refresh_Min){
+				
+				var timeLeft = ((3600 - currentMin) + refresh_Min) * 1000; 
+				
+			}else if(currentMin < refresh_Min){
+				
+				var timeLeft = (refresh_Min - currentMin) * 1000;
+				
+				
+			}
+				setTimeout(function(){location.reload();}, timeLeft);
+				//setInterval(function(){console.log(new Date().getMinutes() * 60 + new Date().getSeconds())}, 1000);
+		}
+		
+		
+		
+		function go(sect, width){
+			
+			var max_sect = $("#caption > div").length;
+			var cur_sect = '#section_'+sect;
+			$('#caption').show();
+			$('#caption').animate({
+			   'left': width
+			}, 1000, 'linear', function(){
+				sect++;
+				width -= $(cur_sect).width();
+				if(sect > max_sect){
+					setTimeout(function(){
+						$('#caption').animate(
+								{'left': width}, 
+								1000, 
+								'linear', 
+								function(){
+									$('#caption').hide(0,'swing', 
+										function(){ 
+											$('#caption').css('left','1920px');
+											sect = 1;
+											width = 0;
+											go(sect, width);
+										})
+								})
+					}, 10000);
+				}else{
+					setTimeout(function(){go(sect, width);}, 10000);
 					
-					sect--;
-					sect = sect < 1 ? max_sect : sect;
-					console.log('scrolling up !', sect);
 				}
-				else{
-					
-					sect++;
-					sect = sect > max_sect ? 1 : sect;
-					console.log('scrolling down !', sect);
-				}
-				
-				
-				$('html, body').animate({
-					   'scrollTop':   $('#section_'+sect).offset().top
-					 }, 1, 'linear', function(){
-						$('#section_'+sect).fadeIn(1000);
-						
-				});
-				
 			});
-
+			
+		}
+		function set_time(){
+			var today = new Date();
+			var currentHr = ('0'+today.getHours()).slice(-2);
+			var currentMin = ('0'+today.getMinutes()).slice(-2);
+			var currentSec = today.getSeconds();
+			$('#time').html(currentHr +':'+ currentMin);
+			var next_refresh_time = 60 - currentSec;
+			setTimeout(function(){
+				set_time();
+			}, next_refresh_time * 1000);
+			
+		}
+		$(document).ready(function(){
+			set_time();
+			refresh();
+			go(sect, width);
 		})
 	</script>
 </head>
 <body>
+
 <div id="caption">
 <?php
 $sectionCount = 1;
-$db_value = 100.11; // value taken from database
+$db_value = $db_data[0]; // value taken from database
 
-if($db_value > 100){
+if($db_value >= 100){
 	$condition = '不良';
 }elseif($db_value < 100 && $db_value > 59){
 	$condition = '普通';
@@ -90,7 +139,7 @@ if($db_value > 100){
 $value = '<span style="color:yellow;">'.$db_value.'</span>';
 $airCondition = '<span style="color:yellow;">'.$condition.'</span>';
 foreach($str AS $paragraph){
-	//$section = explode('|', $section);
+	
 	$div =  "<div id='section_".$sectionCount."' class='sections'>";
 		if(preg_match("/%s年/", $paragraph, $matches) === 1){
 			$year 		= '<span style="color:yellow;">'.date('Y').'</span>';
@@ -102,7 +151,7 @@ foreach($str AS $paragraph){
 			$paragraph 	= sprintf($paragraph, $mth, $day);
 		}
 		if(preg_match("/{TIME}/", $paragraph, $matches) === 1){
-			$time  		= '<span style="color:yellow;">'.date('H:i A').'</span>';
+			$time  		= '<span id="time" style="color:yellow;">'.date('H:i A').'</span>';
 			$paragraph 	= preg_replace('/{TIME}/', $time, $paragraph);
 		}
 		if(preg_match('/{VALUE}/', $paragraph, $matches) === 1){
@@ -122,33 +171,7 @@ foreach($str AS $paragraph){
 	
 }
 ?>
-<!--
-	<div id="section_1">
-	<a name="section_1"></a>
-	<div>花蓮縣環境保護局</div>
-	<div>河川揚塵監測計畫</div>
-	</div>
-	<div id="section_2">
-	<a name="section_2"></a>
-	<div style="font-size:4cm;"><span style="color:yellow;">2016</span>年<span style="color:yellow;">07</span>月<span style="color:yellow;">04</span>日<span style="color:yellow;">13</span>時</div>
-	<div style="font-size:3cm;">懸浮微粒濃度為 <span style="color:yellow;">100.11</span> µg/m<sup>3</sup></div> 
-	</div>
-	<div id="section_3">
-	<a name="section_3"></a>
-	<div>空氣品質<span style="color:yellow;">良好</span></div>
-	<div>揚塵防制不能少</div>
-	</div>
-	<div id="section_4">
-	<a name="section_4"></a>
-	<div>生活環境更美好</div>
-	<div>花蓮縣環境保護局關心您</div>
-	</div>
-	<div id="section_5">
-	<a name="section_5"></a>
-	<div>威陞環境科技股份有限公司</div>
-	<div><img src="logo.jpg">河川揚塵監控系統</div>
-	</div>
--->
+
 </div>
 </body>
 </html>
